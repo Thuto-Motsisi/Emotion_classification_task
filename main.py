b84ab1219a3ddcf0768867124fe88a9723007e59
Thuto-Motsisi <2096335@students.wits.ac.za>
2026-07-03 12:47:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title("Evaluating Pseudo-labeling for Setswana Emotion Classification")
+st.subheader("Information about the study")
+st.write("give info...(detailed)")
+st.markdown("---")
+
+st.subheader("Consent")

80be04cfe66bd07f4fd743613d921f6944cbb413
Thuto-Motsisi <2096335@students.wits.ac.za>
2026-07-03 13:05:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ st.write("give info...(detailed)")
 st.markdown("---")
 
 st.subheader("Consent")
+consent_statements = [
+  "I have read and understood the information sheet.",
+  "I understand that my participation in voluntary, I can skip any sentence I find uncomfortable and I can stop at any time without penalty.",
+  "I understand that no personally identifiable information about me will be collected.",
+  "I confirm that I am a native Setswana speaker between the ages of 18 and 65.",
+  "I consent to the emotion labels and confidence scores I provide to be included in an anonymized dataset and to be released after completion of the researchers MSc study, and no later than 3 years after the data has been collected.",
+  "I consent to participate in this research study."
+]

5b6c75b42958b1937bfd7f11992cf05cd40a1723
Thuto-Motsisi <2096335@students.wits.ac.za>
2026-07-07 11:44:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -100,8 +100,10 @@ if st.session_state.page == "choosing_num_sentences":
   ids_excluded_to_user = [item["sentence_id"] for item in labeled_by_user]
   eligible_sentences = [item for item in sentences_to_label if item["sentence_id"] not in ids_excluded_to_user]
   eligible_sentence_ids = [item["sentence_id"] for item in eligible_sentences]
+  chosen_ids = random.sample(eligible_sentence_ids, num_sentences_selected)
   st.write(f"{eligible_sentences}")
   st.write(f"{eligible_sentence_ids}")
+  st.write(f"{chosen_ids}")
 
 
 

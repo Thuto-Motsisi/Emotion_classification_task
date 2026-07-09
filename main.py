a5a9c6baad591e81908ad3b643b936f5f1471f2f
Thuto-Motsisi <2096335@students.wits.ac.za>
2026-07-09 14:36:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if st.session_state.page == "choosing_num_sentences":
   ids_excluded_to_user = [item["sentence_id"] for item in labeled_by_user]
   eligible_sentences = [item for item in sentences_to_label if item["sentence_id"] not in ids_excluded_to_user]
   eligible_sentence_ids = [item["sentence_id"] for item in eligible_sentences]
-  chosen_ids = random.sample(eligible_sentence_ids, num_sentences_selected)
+  chosen_ids = sorted(random.sample(eligible_sentence_ids, num_sentences_selected))
   if chosen_ids:
     response = supabase.table("sentences").select("sentence").in_("sentence_id",chosen_ids).execute()
     sentences = [row["sentence"] for row in response.data]
